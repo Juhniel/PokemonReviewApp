eb1d124b3e4a4d97fcbc170295143cbb67b420fb
Juhniel <juhn.sweden@gmail.com>
2023-09-18 15:51:51 +0200

    Review + Reviewer Mapping + GetRequest

--- PokemonReviewApp/Helper/MappingProfiles.cs
+++ PokemonReviewApp/Helper/MappingProfiles.cs
@@ -11,6 +11,8 @@ namespace PokemonReviewApp.Helper
             CreateMap<Pokemon, PokemonDto>();
             CreateMap<Category, CategoryDto>();
             CreateMap<Country, CountryDto>();
+            CreateMap<Review, ReviewDto>();
+            CreateMap<Reviewer, ReviewerDto>();
         }
     }
 }
